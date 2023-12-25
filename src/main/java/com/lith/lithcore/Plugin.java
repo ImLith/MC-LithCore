189829b802a4167f74a32e6ecc67bc55d443a11e
ImLith <baranauskas.edvinas26@gmail.com>
2023-12-25 03:27:39 +0200

    init

---
+++ src/main/java/com/lith/lithcore/Plugin.java
@@ -0,0 +1,19 @@
+package com.lith.lithcore;
+
+import java.util.logging.Logger;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ * lithcore java plugin
+ */
+public class Plugin extends JavaPlugin {
+  private static final Logger LOGGER = Logger.getLogger("lithcore");
+
+  public void onEnable() {
+    LOGGER.info("lithcore enabled");
+  }
+
+  public void onDisable() {
+    LOGGER.info("lithcore disabled");
+  }
+}
