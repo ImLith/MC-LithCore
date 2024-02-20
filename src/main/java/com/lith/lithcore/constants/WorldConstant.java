d52385b7f0c9a9da7e870d09dab255dd9c0b0d26
ImLith <baranauskas.edvinas26@gmail.com>
2024-02-20 02:55:10 +0200

    feat: added world constant

---
+++ src/main/java/com/lith/lithcore/constants/WorldConstant.java
@@ -0,0 +1,6 @@
+package main.java.com.lith.lithcore.constants;
+
+public class WorldConstant {
+    public static final int DAY_TIME_MIN_VALUE = 0;
+    public static final int DAY_TIME_MAX_VALUE = 12000;
+}
