31ef01aff2906d1a889c455cc834a42f47d554e9
ImLith <baranauskas.edvinas26@gmail.com>
2024-03-23 23:30:57 +0200

    feat: added AbstractConfigKey
    used to setup config keys
    uses recursion to parse class names as config key names

---
+++ src/main/java/com/lith/lithcore/abstractClasses/AbstractConfigKey.java
@@ -0,0 +1,48 @@
+package com.lith.lithcore.abstractClasses;
+
+public abstract class AbstractConfigKey {
+    private static String path = null;
+
+    protected static String setKey(String key) {
+        return getPath() + key;
+    }
+
+    private static String getPath() {
+        if (path == null)
+            path = calculatePath();
+
+        return path;
+    }
+
+    private static String calculatePath() {
+        StringBuilder path = new StringBuilder();
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+        Class<?> extendingClass = null;
+        for (int i = 1; i < stackTrace.length; i++) {
+            String className = stackTrace[i].getClassName();
+
+            try {
+                Class<?> clazz = Class.forName(className);
+
+                if (AbstractConfigKey.class.isAssignableFrom(clazz) && !AbstractConfigKey.class.equals(clazz)) {
+                    extendingClass = clazz;
+                    break;
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (extendingClass != null) {
+            Class<?> currentClass = extendingClass;
+
+            while (currentClass != null && AbstractConfigKey.class.isAssignableFrom(currentClass)) {
+                path.insert(0, currentClass.getSimpleName().toLowerCase() + ".");
+                currentClass = currentClass.getEnclosingClass();
+            }
+        }
+
+        return path.toString();
+    }
+}
