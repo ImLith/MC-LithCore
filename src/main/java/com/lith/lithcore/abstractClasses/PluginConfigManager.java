d7adf894ba01b3656edc2748f0f5f9afe9830e79
ImLith <baranauskas.edvinas26@gmail.com>
2024-03-24 21:44:39 +0200

    feat: added base methods for getting configs easier

--- src/main/java/com/lith/lithcore/abstractClasses/PluginConfigManager.java
+++ src/main/java/com/lith/lithcore/abstractClasses/PluginConfigManager.java
@@ -2,6 +2,8 @@ package com.lith.lithcore.abstractClasses;
 
 import org.bukkit.configuration.file.FileConfiguration;
 
+import com.lith.lithcore.utils.SupportUtil;
+
 @SuppressWarnings("rawtypes")
 public abstract class PluginConfigManager {
     public final FileConfiguration config;
@@ -13,5 +15,34 @@ public abstract class PluginConfigManager {
 
         plugin.cm = this;
         this.config = plugin.getConfig();
+    }
+
+    protected String getMessage(String key) {
+        return getMessage(key, true, true);
+    }
+
+    protected String getMessage(String key, Boolean addColors) {
+        return getMessage(key, addColors, true);
+    }
+
+    protected String getMessage(String key, Boolean addColors, Boolean addUnicodes) {
+        String text = getString(key);
+
+        if (text == null)
+            return null;
+        if (addColors)
+            text = SupportUtil.addColors(text);
+        if (addUnicodes)
+            text = SupportUtil.addUnicodes(text);
+
+        return text;
+    }
+
+    protected String getString(String key) {
+        return config.getString(key);
+    }
+
+    protected Object getObject(String key) {
+        return config.get(key);
     }
 }
