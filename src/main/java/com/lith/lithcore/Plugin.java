7a6adfa27b0199bad0583096aed71385616d4241
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-13 03:11:33 +0300

    feat: simplified ConfigManager and Plugin usage

--- src/main/java/com/lith/lithcore/Plugin.java
+++ src/main/java/com/lith/lithcore/Plugin.java
@@ -3,13 +3,13 @@ package com.lith.lithcore;
 import com.lith.lithcore.abstractClasses.AbstractPlugin;
 import com.lith.lithcore.config.ConfigManager;
 
-public class Plugin extends AbstractPlugin<Plugin, ConfigManager<Plugin>> {
+public class Plugin extends AbstractPlugin<Plugin, ConfigManager> {
   public static Plugin plugin;
 
   @Override
   public void onEnable() {
     plugin = this;
-    configs = new ConfigManager<Plugin>(this);
+    configs = new ConfigManager(this);
 
     super.onEnable();
   }
--- src/main/java/com/lith/lithcore/config/ConfigManager.java
+++ src/main/java/com/lith/lithcore/config/ConfigManager.java
@@ -1,16 +1,15 @@
 package com.lith.lithcore.config;
 
+import com.lith.lithcore.Plugin;
 import com.lith.lithcore.Static.ConfigKeys;
-import com.lith.lithcore.abstractClasses.AbstractPlugin;
 import lombok.Getter;
 import com.lith.lithcore.abstractClasses.AbstractConfigManager;
 
-public class ConfigManager<P extends AbstractPlugin<P, ConfigManager<P>>>
-        extends AbstractConfigManager<P, ConfigManager<P>> {
+public class ConfigManager extends AbstractConfigManager<Plugin, ConfigManager> {
     @Getter
     private ConfigMessages configMsg;
 
-    public ConfigManager(final P plugin) {
+    public ConfigManager(final Plugin plugin) {
         super(plugin);
     }
 
