0174d92fd5bb05f9df2f74962f9c3b7be777e363
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-14 23:08:32 +0300

    feat: added more flexibality to readl config helper

--- src/main/java/com/lith/lithcore/helpers/ReloadConfigCmd.java
+++ src/main/java/com/lith/lithcore/helpers/ReloadConfigCmd.java
@@ -2,27 +2,30 @@ package com.lith.lithcore.helpers;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import com.lith.lithcore.abstractClasses.AbstractCommand;
 import com.lith.lithcore.abstractClasses.AbstractPlugin;
 
 public class ReloadConfigCmd<P extends AbstractPlugin<P, ?>> extends AbstractCommand<P> {
-    protected final String permission;
-    protected final Boolean onlyPlayer = false;
     protected final String usage;
 
-    public ReloadConfigCmd(@NotNull P plugin, @NotNull String permission, @NotNull String cmd) {
-        super(plugin, cmd);
-
-        this.permission = permission;
-        this.usage = "Use '" + cmd + "' to reload the configs";
-        plugin.getCommand(cmd).setExecutor(this);
+    public ReloadConfigCmd(@NotNull P plugin, String permission, @NotNull String cmd) {
+        this(plugin, permission, cmd, false);
     }
 
-    public ReloadConfigCmd(P plugin, String permission, String cmd, String usage) {
-        super(plugin, cmd);
+    public ReloadConfigCmd(@NotNull P plugin, String permission, @NotNull String cmd, Boolean onlyPlayer) {
+        this(plugin, permission, cmd, "Use '" + cmd + "' to reload the configs", onlyPlayer);
+    }
 
-        this.permission = permission;
+    public ReloadConfigCmd(@NotNull P plugin, String permission, @NotNull String cmd, @NotNull String usage) {
+        this(plugin, permission, cmd, usage, false);
+    }
+
+    public ReloadConfigCmd(@NotNull P plugin, String permission, @NotNull String cmd, @NotNull String usage,
+            Boolean onlyPlayer) {
+        super(plugin, cmd, permission, onlyPlayer);
+
         this.usage = usage;
         plugin.getCommand(cmd).setExecutor(this);
     }
@@ -31,6 +34,10 @@ public class ReloadConfigCmd<P extends AbstractPlugin<P, ?>> extends AbstractCom
     public boolean onExecute(CommandSender sender, Command command, String label, String[] args) {
         plugin.reloadConfigs();
 
+        if ((sender instanceof Player))
+            sender.sendMessage("Configs reloaded!");
+
+        plugin.log.info("Configs reloaded!");
         return true;
     }
 
