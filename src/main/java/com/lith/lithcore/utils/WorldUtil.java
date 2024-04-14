294198e38c64352255b9f47d4e404e88f290769c
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-14 21:35:53 +0300

    feat: wolrdUtil to set daynight cycle gamerule

--- src/main/java/com/lith/lithcore/utils/WorldUtil.java
+++ src/main/java/com/lith/lithcore/utils/WorldUtil.java
@@ -1,5 +1,10 @@
 package com.lith.lithcore.utils;
 
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameRule;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 import com.lith.lithcore.constants.WorldConstant;
@@ -13,5 +18,20 @@ public final class WorldUtil {
 
     public static boolean isNight(@NotNull final World world) {
         return (!(WorldUtil.isDay(world)));
+    }
+
+    public static void setDaylightCycle(boolean value) {
+        setDaylightCycle(value, null);
+    }
+
+    public static void setDaylightCycle(boolean value, Set<String> forWorlds) {
+        Stream<World> worlds = Bukkit.getWorlds().stream();
+
+        if (forWorlds != null)
+            worlds = worlds.filter(world -> forWorlds.contains(world.getName()));
+
+        worlds.forEach(world -> {
+            world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, value);
+        });
     }
 }
