c3af5430aaf6f17cc7b8fb3dac4f408dfd9d1621
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-14 19:27:04 +0300

    fix: LightConstant MAX value

--- src/main/java/com/lith/lithcore/constants/LightConstant.java
+++ src/main/java/com/lith/lithcore/constants/LightConstant.java
@@ -2,5 +2,5 @@ package com.lith.lithcore.constants;
 
 public final class LightConstant {
     public static final int MIN = 0;
-    public static final int MAX = 0;
+    public static final int MAX = 15;
 }
